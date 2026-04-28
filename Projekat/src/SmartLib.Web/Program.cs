dd3625a447e019a86e42f358487598449567237c
ImroVlaja <ivlajcic1@etf.unsa.ba>
2026-04-28 21:28:27 +0200

    Prelazak sistema na Database (#2)
    Co-authored-by: ImranVlajcic <imranvlajcic04@gmail.com>

--- Projekat/src/SmartLib.Web/Program.cs
+++ Projekat/src/SmartLib.Web/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
 using SmartLib.Core.Interfaces;
+using SmartLib.Infrastructure.Data;
 using SmartLib.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +9,10 @@ var builder = WebApplication.CreateBuilder(args);
 // --------------------
 // SERVISI
 // --------------------
+
+// Database
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllersWithViews();
 
@@ -30,6 +36,13 @@ builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationSc
 builder.Services.AddAuthorization();
 
 var app = builder.Build();
+
+// Auto-migrate na startu
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.Migrate();
+}
 
 // --------------------
 // MIDDLEWARE
